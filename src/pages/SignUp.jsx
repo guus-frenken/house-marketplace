fae3a96e4319c8d22520a045df85dd386f4fe0c1
Guus Frenken <g.frenken@outlook.com>
2021-12-29 13:57:06 +0100

    Add ability to register and sign in with Google

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -4,6 +4,7 @@ import {Link, useNavigate} from 'react-router-dom';
 import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import {setDoc, doc, serverTimestamp} from 'firebase/firestore';
 import {db} from '../firebase.config';
+import OAuth from '../components/OAuth';
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -97,6 +98,7 @@ function SignUp() {
                         </button>
                     </div>
                 </form>
+                <OAuth />
                 <Link to='/sign-in' className='registerLink'>
                     Sign In Instead
                 </Link>
